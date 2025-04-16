29ba6655f633ea654a72405bf5980566160bbea3
AnnaYurch <anna.yurch2005@gmail.com>
2025-04-16 04:01:36 +0300

    b_i03

--- arithmetic/big_integer/include/big_int.h
+++ arithmetic/big_integer/include/big_int.h
@@ -1,3 +1,6 @@
+//
+// Created by Des Caldnd on 5/27/2024.
+//
 #ifndef MP_OS_BIG_INT_H
 #define MP_OS_BIG_INT_H
 
@@ -11,143 +14,160 @@
 #include <pp_allocator.h>
 #include <not_implemented.h>
 
-namespace __detail {
-	constexpr unsigned int generate_half_mask() {
-		unsigned int res = 0;
+inline constexpr unsigned long long BASE = 1ULL << (8 * sizeof(unsigned int));
 
-		for (size_t i = 0; i < sizeof(unsigned int) * 4; ++i) {
-			res |= (1u << i);
-		}
+namespace __detail
+{
+    constexpr unsigned int generate_half_mask() {
+        unsigned int res = 0;
 
-		return res;
-	}
+        for(size_t i = 0; i < sizeof(unsigned int) * 4; ++i) {
+            res |= (1u << i);
+        }
 
-	constexpr size_t nearest_greater_power_of_2(size_t size) noexcept {
-		int ones_counter = 0, index = -1;
+        return res;
+    }
 
-		constexpr const size_t o = 1;
+    constexpr size_t nearest_greater_power_of_2(size_t size) noexcept
+    {
+        int ones_counter = 0, index = -1;
 
-		for (int i = sizeof(size_t) * 8 - 1; i >= 0; --i) {
-			if (size & (o << i)) {
-				if (ones_counter == 0)
-					index = i;
-				++ones_counter;
-			}
-		}
+        constexpr const size_t o = 1;
 
-		return ones_counter <= 1 ? (1u << index) : (1u << (index + 1));
-	}
-}// namespace __detail
+        for (int i = sizeof(size_t) * 8 - 1; i >= 0; --i)
+        {
+            if (size & (o << i))
+            {
+                if (ones_counter == 0)
+                    index = i;
+                ++ones_counter;
+            }
+        }
 
-class big_int {
-	// Call optimise after every operation!!!
-	bool _sign;// 1 +  0 -
-	std::vector<unsigned int, pp_allocator<unsigned int>> _digits;
+        return ones_counter <= 1 ? (1u << index) : (1u << (index + 1));
+    }
+}
+
+class big_int
+{
+    // Call optimise after every operation!!!
+    bool _sign; // 1 +  0 -
+    std::vector<unsigned int, pp_allocator<unsigned int>> _digits;
 
 public:
-	enum class multiplication_rule {
-		trivial,
-		Karatsuba,
-		SchonhageStrassen
-	};
 
-	enum class division_rule {
-		trivial,
-		Newton,
-		BurnikelZiegler
-	};
+    enum class multiplication_rule
+    {
+        trivial,
+        Karatsuba,
+        SchonhageStrassen
+    };
+
+    enum class division_rule
+    {
+        trivial,
+        Newton,
+        BurnikelZiegler
+    };
 
 private:
-	/** Decides type of mult/div that depends on size of lhs and rhs
+
+    /** Decides type of mult/div that depends on size of lhs and rhs
      */
-	multiplication_rule decide_mult(size_t rhs) const noexcept;
-	division_rule decide_div(size_t rhs) const noexcept;
+    multiplication_rule decide_mult(size_t rhs) const noexcept;
+    division_rule decide_div(size_t rhs) const noexcept;
 
 public:
-	using value_type = unsigned int;
 
-	template<class alloc>
-	explicit big_int(const std::vector<unsigned int, alloc>& digits, bool sign = true, pp_allocator<unsigned int> allocator = pp_allocator<unsigned int>());
+    using value_type = unsigned int;
 
-	explicit big_int(const std::vector<unsigned int, pp_allocator<unsigned int>>& digits, bool sign = true);
+    template<class alloc>
+    explicit big_int(const std::vector<unsigned int, alloc> &digits, bool sign = true, pp_allocator<unsigned int> allocator = pp_allocator<unsigned int>());
 
-	explicit big_int(std::vector<unsigned int, pp_allocator<unsigned int>>&& digits, bool sign = true) noexcept;
+    explicit big_int(const std::vector<unsigned int, pp_allocator<unsigned int>> &digits, bool sign = true);
 
-	explicit big_int(const std::string& num, unsigned int radix = 10, pp_allocator<unsigned int> = pp_allocator<unsigned int>());
+    explicit big_int(std::vector<unsigned int, pp_allocator<unsigned int>> &&digits, bool sign = true) noexcept;
 
-	template<std::integral Num>
-	big_int(Num d, pp_allocator<unsigned int> = pp_allocator<unsigned int>());
+    explicit big_int(const std::string& num, unsigned int radix = 10, pp_allocator<unsigned int> = pp_allocator<unsigned int>());
 
-	big_int(pp_allocator<unsigned int> = pp_allocator<unsigned int>());
+    template<std::integral Num>
+    big_int(Num d, pp_allocator<unsigned int> = pp_allocator<unsigned int>());
 
-	explicit operator bool() const noexcept;//false if 0 , else true
+    big_int(pp_allocator<unsigned int> = pp_allocator<unsigned int>());
 
-	big_int& operator++() &;
-	big_int operator++(int);
+    explicit operator bool() const noexcept; //false if 0 , else true
 
-	big_int& operator--() &;
-	big_int operator--(int);
+    big_int& operator++() &;
+    big_int operator++(int);
 
-	big_int& operator+=(const big_int& other) &;
+    big_int& operator--() &;
+    big_int operator--(int);
 
-	/** Shift will be needed for multiplication implementation
+    big_int& operator+=(const big_int& other) &;
+
+    /** Shift will be needed for multiplication implementation
      *  @example Shift = 0: 111 + 222 = 333
      *  @example Shift = 1: 111 + 222 = 2331
      */
-	big_int& plus_assign(const big_int& other, size_t shift = 0) &;
+    big_int& plus_assign(const big_int& other, size_t shift = 0) &;
 
 
-	big_int& operator-=(const big_int& other) &;
+    big_int& operator-=(const big_int& other) &;
 
-	big_int& minus_assign(const big_int& other, size_t shift = 0) &;
+    big_int& minus_assign(const big_int& other, size_t shift = 0) &;
 
-	/** Delegates to multiply_assign and calls decide_mult
+    /** Delegates to multiply_assign and calls decide_mult
      */
-	big_int& operator*=(const big_int& other) &;
+    big_int& operator*=(const big_int& other) &;
 
-	big_int& multiply_assign(const big_int& other, multiplication_rule rule = multiplication_rule::trivial) &;
+    big_int& multiply_assign(const big_int& other, multiplication_rule rule = multiplication_rule::trivial) &;
 
-	big_int& operator/=(const big_int& other) &;
+    big_int& operator/=(const big_int& other) &;
 
-	big_int& divide_assign(const big_int& other, division_rule rule = division_rule::trivial) &;
+    big_int& divide_assign(const big_int& other, division_rule rule = division_rule::trivial) &;
 
-	big_int& operator%=(const big_int& other) &;
+    big_int& operator%=(const big_int& other) &;
 
-	big_int& modulo_assign(const big_int& other, division_rule rule = division_rule::trivial) &;
+    big_int& modulo_assign(const big_int& other, division_rule rule = division_rule::trivial) &;
 
-	big_int operator+(const big_int& other) const;
-	big_int operator-(const big_int& other) const;
-	big_int operator*(const big_int& other) const;
-	big_int operator/(const big_int& other) const;
-	big_int operator%(const big_int& other) const;
+    big_int operator+(const big_int& other) const;
+    big_int operator-(const big_int& other) const;
+    big_int operator*(const big_int& other) const;
+    big_int operator/(const big_int& other) const;
+    big_int operator%(const big_int& other) const;
 
-	std::strong_ordering operator<=>(const big_int& other) const noexcept;
+    std::strong_ordering operator<=>(const big_int& other) const noexcept;
 
-	bool operator==(const big_int& other) const noexcept;
+    bool operator==(const big_int& other) const noexcept;
 
-	big_int& operator<<=(size_t shift) &;
-	big_int& operator>>=(size_t shift) &;
+    big_int& operator<<=(size_t shift) &;
+
+    big_int& operator>>=(size_t shift) &;
 
 
-	big_int operator<<(size_t shift) const;
-	big_int operator>>(size_t shift) const;
+    big_int operator<<(size_t shift) const;
+    big_int operator>>(size_t shift) const;
 
-	big_int operator~() const;
+    big_int operator~() const;
 
-	big_int& operator&=(const big_int& other) &;
-	big_int& operator|=(const big_int& other) &;
-	big_int& operator^=(const big_int& other) &;
+    big_int& operator&=(const big_int& other) &;
 
-	big_int operator&(const big_int& other) const;
-	big_int operator|(const big_int& other) const;
-	big_int operator^(const big_int& other) const;
+    big_int& operator|=(const big_int& other) &;
 
-	friend std::ostream& operator<<(std::ostream& stream, big_int const& value);
-	friend std::istream& operator>>(std::istream& stream, big_int& value);
+    big_int& operator^=(const big_int& other) &;
 
-	std::string to_string() const;
 
-	friend big_int multiply_karatsuba(const big_int &a, const big_int &b);
+    big_int operator&(const big_int& other) const;
+    big_int operator|(const big_int& other) const;
+    big_int operator^(const big_int& other) const;
+
+    friend std::ostream &operator<<(std::ostream &stream, big_int const &value);
+
+    friend std::istream &operator>>(std::istream &stream, big_int &value);
+
+    std::string to_string() const;
+
+    friend big_int multiply_karatsuba(const big_int &a, const big_int &b);
 };
 
 template<class alloc>
@@ -182,4 +202,4 @@ big_int::big_int(Num d, pp_allocator<unsigned int> allocator) : _sign(d >= 0), _
 
 big_int operator""_bi(unsigned long long n);
 
-#endif
+#endif //MP_OS_BIG_INT_H
